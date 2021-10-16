c553a96d05bf80c1be35549646876b8aa81606d0
Duc Nguyen <minhduc140583@gmail.com>
2021-10-16 14:49:14 +0700

    Refactor code

--- cache/cache_service.go
+++ cache/cache_service.go
@@ -5,8 +5,13 @@ import (
 	"time"
 )
 
+type CachePort interface {
+	Put(ctx context.Context, key string, obj interface{}, timeToLive time.Duration) error
+	Get(ctx context.Context, key string) (string, error)
+}
+
 type CacheService interface {
-	Put(ctx context.Context, key string, obj string, timeToLive time.Duration) error
+	Put(ctx context.Context, key string, obj interface{}, timeToLive time.Duration) error
 	Expire(ctx context.Context, key string, timeToLive time.Duration) (bool, error)
 	Get(ctx context.Context, key string) (string, error)
 	ContainsKey(ctx context.Context, key string) (bool, error)
--- cache/memory_cache_service.go
+++ cache/memory_cache_service.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"time"
 	"unsafe"
@@ -11,7 +12,21 @@ type MemoryCacheService struct {
 	client *Client
 	close  chan struct{}
 }
-
+func NewCacheService(size int64, cleaningEnable bool, cleaningInterval time.Duration) (*MemoryCacheService, error) {
+	return NewMemoryCacheService(size, cleaningEnable, cleaningInterval)
+}
+func NewCacheAdapterByConfig(conf CacheConfig) (*MemoryCacheService, error) {
+	return NewMemoryCacheService(conf.Size, conf.CleaningEnable, conf.CleaningInterval)
+}
+func NewCacheAdapter(size int64, cleaningEnable bool, cleaningInterval time.Duration) (*MemoryCacheService, error) {
+	return NewMemoryCacheService(size, cleaningEnable, cleaningInterval)
+}
+func NewMemoryCacheAdapterByConfig(conf CacheConfig) (*MemoryCacheService, error) {
+	return NewMemoryCacheService(conf.Size, conf.CleaningEnable, conf.CleaningInterval)
+}
+func NewMemoryCacheAdapter(size int64, cleaningEnable bool, cleaningInterval time.Duration) (*MemoryCacheService, error) {
+	return NewMemoryCacheService(size, cleaningEnable, cleaningInterval)
+}
 func NewMemoryCacheServiceByConfig(conf CacheConfig) (*MemoryCacheService, error) {
 	return NewMemoryCacheService(conf.Size, conf.CleaningEnable, conf.CleaningInterval)
 }
@@ -104,14 +119,21 @@ func (c *MemoryCacheService) ContainsKey(ctx context.Context, key string) (bool,
 	return true, nil
 }
 
-// Put new record set key and value
-func (c *MemoryCacheService) Put(ctx context.Context, key string, value string, expire time.Duration) error {
+func (c *MemoryCacheService) Put(ctx context.Context, key string, value interface{}, expire time.Duration) error {
 	if expire == 0 {
 		expire = 24 * time.Hour
 	}
-
+	var v string
+	v, ok := value.(string)
+	if ok == false {
+		json, err := json.Marshal(value)
+		if err != nil {
+			return err
+		}
+		v = string(json)
+	}
 	if err := c.client.Push(key, Item{
-		Data:    value,
+		Data:    v,
 		Expires: time.Now().Add(expire).UnixNano(),
 	}); err != nil {
 		return err
@@ -150,7 +172,6 @@ func (c *MemoryCacheService) Clear(ctx context.Context) error {
 	return nil
 }
 
-// Count return number of records
 func (c *MemoryCacheService) Count(ctx context.Context) (int64, error) {
 	return int64(c.client.GetNumberOfKeys()), nil
 }
@@ -159,12 +180,10 @@ func (c *MemoryCacheService) Keys(ctx context.Context) ([]string, error) {
 	return c.client.Getkeys(), nil
 }
 
-// GetDBSize method return redis database size
 func (c *MemoryCacheService) Size(ctx context.Context) (int64, error) {
 	return int64(unsafe.Sizeof(c.client)), nil
 }
 
-// Close closes the cache and frees up resources.
 func (c *MemoryCacheService) Close(ctx context.Context) error {
 	c.close <- struct{}{}
 	c.client = NewClient(10*1024*1024, true) // 10 * 1024 * 1024 for 10 mb
